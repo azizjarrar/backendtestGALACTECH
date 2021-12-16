e443fe1fd9f850c29b89a3c00708b081802b066c
aziz jarrar <tripelbil@gmail.com>
2021-12-16 15:29:03 +0100

    change to heroko

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const app = require('./app')
 const server = http.createServer(app)
-const port = process.env.Port || 5010
+const port = process.env.PORT || 5010
 server.listen(port,()=>{
     console.log("server online")
   })
